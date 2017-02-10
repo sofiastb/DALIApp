fe3837d2e3fbd86e13f60a9d37128de326544d66
sofiastb <sofiastb27@gmail.com>
2017-02-10 13:32:11 -0500

    Added Firebase

---
+++ DALIApp/Member.swift
@@ -0,0 +1,17 @@
+//
+//  Member.swift
+//  DALIApp
+//
+//  Created by Sofia Stanescu-Bellu on 2/10/17.
+//  Copyright © 2017 sofiastb. All rights reserved.
+//
+
+import Foundation
+import FirebaseDa
+
+class Member {
+    
+    let name: String
+    
+    
+}
---
+++ DALIApp/MemberDescriptionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MemberDescriptionViewController.swift
+//  DALIApp
+//
+//  Created by Sofia Stanescu-Bellu on 2/10/17.
+//  Copyright © 2017 sofiastb. All rights reserved.
+//
+
+import UIKit
+
+class MemberDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
